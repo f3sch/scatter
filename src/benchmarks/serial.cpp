1ffd6c415cd99f1c837b3ea2cc9a02c91b40dc48
Anton Kreuzkamp <af259@stud.uni-heidelberg.de>
2022-01-28 19:06:43 +0100

    Add a serialSimpleId and copy benchmark
    Adds one benchmark using the naive scatter with contigous indices and
    one benchmark with a simple std::copy to use as baseline.

--- src/benchmarks/serial.cpp
+++ src/benchmarks/serial.cpp
@@ -3,6 +3,46 @@
 #include <benchmark/benchmark.h>
 #include <vector>
 
+static void benchCopy(benchmark::State &state) {
+  auto N = state.range(0);
+  pad::benchmarks::DataVec vec(N);
+  pad::benchmarks::IndexVec index(N);
+  pad::benchmarks::DataVec out(N);
+
+  std::iota(vec.begin(), vec.end(), 0);
+  std::iota(index.begin(), index.end(), 0);
+
+  for (auto _ : state) {
+    std::copy(vec.begin(), vec.end(), out.begin());
+    benchmark::DoNotOptimize(vec.data());
+    benchmark::DoNotOptimize(index.data());
+    benchmark::ClobberMemory();
+  }
+  pad::benchmarks::verifyScatter(vec, index, out);
+  pad::benchmarks::benchCounters(state);
+}
+BENCHMARK(benchCopy)->Apply(pad::benchmarks::benchArgs)->UseRealTime();
+
+static void benchSerialSimpleId(benchmark::State &state) {
+  auto N = state.range(0);
+  pad::benchmarks::DataVec vec(N);
+  pad::benchmarks::IndexVec index(N);
+  pad::benchmarks::DataVec out(N);
+
+  std::iota(vec.begin(), vec.end(), 0);
+  std::iota(index.begin(), index.end(), 0);
+
+  for (auto _ : state) {
+    pad::serial::scatter(out.begin(), vec, index);
+    benchmark::DoNotOptimize(vec.data());
+    benchmark::DoNotOptimize(index.data());
+    benchmark::ClobberMemory();
+  }
+  pad::benchmarks::verifyScatter(vec, index, out);
+  pad::benchmarks::benchCounters(state);
+}
+BENCHMARK(benchSerialSimpleId)->Apply(pad::benchmarks::benchArgs)->UseRealTime();
+
 static void benchSerialSimple(benchmark::State &state)
 {
   auto [vec, index] = pad::benchmarks::makeData(state.range(0));
