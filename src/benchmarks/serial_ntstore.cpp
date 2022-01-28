f191aaa30cb947eacad609e775c4ddff316de014
f3sch <f3sch.git@outlook.com>
2022-01-28 17:40:04 +0100

    partially reverting previous commit...
    sorry, didnt realize movntia64 need long long int
    But would that be cheating since in all other benchmarks
    we are operating on floating point registers and here we are
    not?
    
    Signed-off-by: f3sch <f3sch.git@outlook.com>

--- src/benchmarks/serial_ntstore.cpp
+++ src/benchmarks/serial_ntstore.cpp
@@ -5,8 +5,8 @@
 
 static void benchSerialSimple(benchmark::State &state)
 {
-  auto [vec, index] = pad::benchmarks::makeData(state.range(0));
-  pad::benchmarks::DataVec out(state.range(0));
+  auto [vec, index] = pad::benchmarks::makeData<long long int>(state.range(0));
+  pad::benchmarks::Vec<long long int> out(state.range(0));
   for (auto _ : state) {
     pad::serial_ntstore::scatter(out.begin(), vec, index);
     benchmark::DoNotOptimize(vec.data());
