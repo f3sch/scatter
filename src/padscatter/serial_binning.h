6da6519bf43b70ae7ae8fc83c3cba213aad04d22
Anton Kreuzkamp <af259@stud.uni-heidelberg.de>
2022-01-29 14:00:55 +0100

    Add optional debug output to serial_binning
    and fix two critical issues in the algorithms which I found that way

--- src/padscatter/serial_binning.h
+++ src/padscatter/serial_binning.h
@@ -14,6 +14,14 @@
 #include <range/v3/algorithm/fill.hpp>
 #include <span>
 #include <random>
+#include <iostream>
+
+//#define DEBUG
+#ifdef DEBUG
+#define LOG(code) code
+#else
+#define LOG(Code)
+#endif
 
 namespace pad::serial_binning
 {
@@ -68,14 +76,14 @@ void scatter(OutIt_t outIt, const InRng_t &inRng, const IdxRng_t &idxRng)
   Idx_t bin5Tag = empty_tag;
   Idx_t bin6Tag = empty_tag;
   Idx_t bin7Tag = empty_tag;
-  uint_fast8_t bin0FillLevel = 0;
-  uint_fast8_t bin1FillLevel = 0;
-  uint_fast8_t bin2FillLevel = 0;
-  uint_fast8_t bin3FillLevel = 0;
-  uint_fast8_t bin4FillLevel = 0;
-  uint_fast8_t bin5FillLevel = 0;
-  uint_fast8_t bin6FillLevel = 0;
-  uint_fast8_t bin7FillLevel = 0;
+  size_t bin0FillLevel = 0;
+  size_t bin1FillLevel = 0;
+  size_t bin2FillLevel = 0;
+  size_t bin3FillLevel = 0;
+  size_t bin4FillLevel = 0;
+  size_t bin5FillLevel = 0;
+  size_t bin6FillLevel = 0;
+  size_t bin7FillLevel = 0;
 
   std::uniform_int_distribution dist{ 0, 7 };
   std::mt19937 random_engine{ std::random_device{}() };
@@ -87,108 +95,132 @@ void scatter(OutIt_t outIt, const InRng_t &inRng, const IdxRng_t &idxRng)
       if (tag == bin0Tag) {
         bin0[bin0FillLevel] = { idx, val };
         ++bin0FillLevel;
+        LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into bin0, " << (size_t)bin0FillLevel << "/" << binSize << std::endl);
 
         if (bin0FillLevel == binSize) {
           flush(bin0);
           bin0FillLevel = 0;
           bin0Tag = empty_tag;
+          LOG(std::cout << "Bin0 full. Flushed." << std::endl);
         }
       } else if (tag == bin1Tag) {
         bin1[bin1FillLevel] = { idx, val };
         ++bin1FillLevel;
+        LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into bin1, " << (size_t)bin1FillLevel << "/" << binSize << std::endl);
 
         if (bin1FillLevel == binSize) {
           flush(bin1);
           bin1FillLevel = 1;
           bin1Tag = empty_tag;
+          LOG(std::cout << "Bin1 full. Flushed." << std::endl);
         }
       } else if (tag == bin2Tag) {
         bin2[bin2FillLevel] = { idx, val };
         ++bin2FillLevel;
+        LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into bin2, " << (size_t)bin2FillLevel << "/" << binSize << std::endl);
 
         if (bin2FillLevel == binSize) {
           flush(bin2);
           bin2FillLevel = 2;
           bin2Tag = empty_tag;
+          LOG(std::cout << "Bin2 full. Flushed." << std::endl);
         }
       } else if (tag == bin3Tag) {
         bin3[bin3FillLevel] = { idx, val };
         ++bin3FillLevel;
+        LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into bin3, " << (size_t)bin3FillLevel << "/" << binSize << std::endl);
 
         if (bin3FillLevel == binSize) {
           flush(bin3);
           bin3FillLevel = 3;
           bin3Tag = empty_tag;
+          LOG(std::cout << "Bin3 full. Flushed." << std::endl);
         }
       } else if (tag == bin4Tag) {
         bin4[bin4FillLevel] = { idx, val };
         ++bin4FillLevel;
+        LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into bin4, " << (size_t)bin4FillLevel << "/" << binSize << std::endl);
 
         if (bin4FillLevel == binSize) {
           flush(bin4);
           bin4FillLevel = 4;
           bin4Tag = empty_tag;
+          LOG(std::cout << "Bin4 full. Flushed." << std::endl);
         }
       } else if (tag == bin5Tag) {
         bin5[bin5FillLevel] = { idx, val };
         ++bin5FillLevel;
+        LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into bin5, " << (size_t)bin5FillLevel << "/" << binSize << std::endl);
 
         if (bin5FillLevel == binSize) {
           flush(bin5);
           bin5FillLevel = 5;
           bin5Tag = empty_tag;
+          LOG(std::cout << "Bin5 full. Flushed." << std::endl);
         }
       } else if (tag == bin6Tag) {
         bin6[bin6FillLevel] = { idx, val };
         ++bin6FillLevel;
+        LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into bin6, " << (size_t)bin6FillLevel << "/" << binSize << std::endl);
 
         if (bin6FillLevel == binSize) {
           flush(bin6);
           bin6FillLevel = 6;
           bin6Tag = empty_tag;
+          LOG(std::cout << "Bin6 full. Flushed." << std::endl);
         }
       } else if (tag == bin7Tag) {
         bin7[bin7FillLevel] = { idx, val };
         ++bin7FillLevel;
+        LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into bin7, " << (size_t)bin7FillLevel << "/" << binSize << std::endl);
 
         if (bin7FillLevel == binSize) {
           flush(bin7);
           bin7FillLevel = 7;
           bin7Tag = empty_tag;
+          LOG(std::cout << "Bin7 full. Flushed." << std::endl);
         }
       } else {
         if (bin0FillLevel == 0) {
           bin0[0] = { idx, val };
           bin0Tag = tag;
           bin0FillLevel = 1;
+          LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into free bin0," << std::endl);
         } else if (bin1FillLevel == 0) {
           bin1[0] = { idx, val };
           bin1Tag = tag;
           bin1FillLevel = 1;
+          LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into free bin1," << std::endl);
         } else if (bin2FillLevel == 0) {
           bin2[0] = { idx, val };
           bin2Tag = tag;
           bin2FillLevel = 1;
+          LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into free bin2," << std::endl);
         } else if (bin3FillLevel == 0) {
           bin3[0] = { idx, val };
           bin3Tag = tag;
           bin3FillLevel = 1;
+          LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into free bin3," << std::endl);
         } else if (bin4FillLevel == 0) {
           bin4[0] = { idx, val };
           bin4Tag = tag;
           bin4FillLevel = 1;
+          LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into free bin4," << std::endl);
         } else if (bin5FillLevel == 0) {
           bin5[0] = { idx, val };
           bin5Tag = tag;
           bin5FillLevel = 1;
+          LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into free bin5," << std::endl);
         } else if (bin6FillLevel == 0) {
           bin6[0] = { idx, val };
           bin6Tag = tag;
           bin6FillLevel = 1;
+          LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into free bin6," << std::endl);
         } else if (bin7FillLevel == 0) {
           bin7[0] = { idx, val };
           bin7Tag = tag;
           bin7FillLevel = 1;
+          LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into free bin7," << std::endl);
         } else {
           auto randomBin = dist(random_engine);
           if (randomBin == 0) {
@@ -196,41 +228,49 @@ void scatter(OutIt_t outIt, const InRng_t &inRng, const IdxRng_t &idxRng)
             bin0[0] = { idx, val };
             bin0Tag = tag;
             bin0FillLevel = 1;
+            LOG(std::cout << "idx: " << idx << ", tag: " << tag << ". All bins occupied, randomly flushing bin0 (" << (size_t)bin0FillLevel << "/" << binSize << ")" << std::endl);
           } else if (randomBin == 1) {
             flush(ranges::views::take_exactly(bin1, bin1FillLevel));
             bin1[0] = { idx, val };
             bin1Tag = tag;
             bin1FillLevel = 1;
+            LOG(std::cout << "idx: " << idx << ", tag: " << tag << ". All bins occupied, randomly flushing bin1 (" << (size_t)bin1FillLevel << "/" << binSize << ")" << std::endl);
           } else if (randomBin == 2) {
             flush(ranges::views::take_exactly(bin2, bin2FillLevel));
             bin2[0] = { idx, val };
             bin2Tag = tag;
             bin2FillLevel = 1;
+            LOG(std::cout << "idx: " << idx << ", tag: " << tag << ". All bins occupied, randomly flushing bin2 (" << (size_t)bin2FillLevel << "/" << binSize << ")" << std::endl);
           } else if (randomBin == 3) {
             flush(ranges::views::take_exactly(bin3, bin3FillLevel));
             bin3[0] = { idx, val };
             bin3Tag = tag;
             bin3FillLevel = 1;
+            LOG(std::cout << "idx: " << idx << ", tag: " << tag << ". All bins occupied, randomly flushing bin3 (" << (size_t)bin3FillLevel << "/" << binSize << ")" << std::endl);
           } else if (randomBin == 4) {
             flush(ranges::views::take_exactly(bin4, bin4FillLevel));
             bin4[0] = { idx, val };
             bin4Tag = tag;
             bin4FillLevel = 1;
+            LOG(std::cout << "idx: " << idx << ", tag: " << tag << ". All bins occupied, randomly flushing bin4 (" << (size_t)bin4FillLevel << "/" << binSize << ")" << std::endl);
           } else if (randomBin == 5) {
             flush(ranges::views::take_exactly(bin5, bin5FillLevel));
             bin5[0] = { idx, val };
             bin5Tag = tag;
             bin5FillLevel = 1;
+            LOG(std::cout << "idx: " << idx << ", tag: " << tag << ". All bins occupied, randomly flushing bin5 (" << (size_t)bin5FillLevel << "/" << binSize << ")" << std::endl);
           } else if (randomBin == 6) {
             flush(ranges::views::take_exactly(bin6, bin6FillLevel));
             bin6[0] = { idx, val };
             bin6Tag = tag;
             bin6FillLevel = 1;
+            LOG(std::cout << "idx: " << idx << ", tag: " << tag << ". All bins occupied, randomly flushing bin6 (" << (size_t)bin6FillLevel << "/" << binSize << ")" << std::endl);
           } else if (randomBin == 7) {
             flush(ranges::views::take_exactly(bin7, bin7FillLevel));
             bin7[0] = { idx, val };
             bin7Tag = tag;
             bin7FillLevel = 1;
+            LOG(std::cout << "idx: " << idx << ", tag: " << tag << ". All bins occupied, randomly flushing bin7 (" << (size_t)bin7FillLevel << "/" << binSize << ")" << std::endl);
           }
         }
       }
@@ -276,8 +316,7 @@ void scatter(OutIt_t outIt, const InRng_t &inRng, const IdxRng_t &idxRng)
   };
 
   static constexpr auto logBinSize = pad::serial_binning::detail::log2(binSize);
-  static constexpr auto bankMask =
-      bankCount - 1; // e.g. 8 - 1 == 7 == 0b00000111
+  static constexpr auto bankMask = bankCount - 1; // e.g. 8 - 1 == 7 == 0b00000111
   auto valChunks = chunk(inRng, chunkSize);
   auto idxChunks = chunk(idxRng, chunkSize);
 
@@ -290,7 +329,7 @@ void scatter(OutIt_t outIt, const InRng_t &inRng, const IdxRng_t &idxRng)
     for (auto &&[val, idx] : zip(valChunk, idxChunk)) {
       assert(0 <= idx && static_cast<size_t>(idx) < ranges::size(idxRng));
       auto tag = idx >> logBinSize;
-      auto bank = idx & bankMask;
+      auto bank = tag & bankMask;
 
       auto cacheTag = cacheTags[bank];
       auto fillLevel = cacheFillLevels[bank];
@@ -299,7 +338,9 @@ void scatter(OutIt_t outIt, const InRng_t &inRng, const IdxRng_t &idxRng)
         valCache[bank][0] = val;
         cacheFillLevels[bank] = 1;
         cacheTags[bank] = tag;
+        LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into free bank" << bank << "," << std::endl);
       } else if (cacheTag != tag) {
+        LOG(std::cout << "idx: " << idx << ", tag: " << tag << ". Bank " << bank << "occupied, flushing." << (size_t)cacheFillLevels[bank] << "/" << binSize << ")" << std::endl);
         flush(idxCache[bank], valCache[bank], fillLevel);
         idxCache[bank][0] = idx;
         valCache[bank][0] = val;
@@ -309,10 +350,12 @@ void scatter(OutIt_t outIt, const InRng_t &inRng, const IdxRng_t &idxRng)
         idxCache[bank][fillLevel] = idx;
         valCache[bank][fillLevel] = val;
         ++cacheFillLevels[bank];
+        LOG(std::cout << "idx: " << idx << ", tag: " << tag << " into bank " << bank << ", " << (size_t)cacheFillLevels[bank] << "/" << binSize << std::endl);
 
-        if (fillLevel == cacheLineSize) {
+        if (fillLevel == cacheLineSize - 1) { // -1 because fillLevel is the old value but lives in a register. As cacheLineSize is a compiletime value it's faster this way than comparing cacheFillLevels[bank] #prematureoptimization
           flush(idxCache[bank], valCache[bank], fillLevel);
           cacheFillLevels[bank] = 0;
+          LOG(std::cout << "Bank full. Flushed." << std::endl);
         }
       }
     }
