5657994ece1e77f98d922911874f258404f95b59
f3sch <f3sch.git@outlook.com>
2022-01-31 11:59:13 +0100

    properly named serial_ntstore
    Signed-off-by: f3sch <f3sch.git@outlook.com>

--- src/benchmarks/serial_ntstore.cpp
+++ src/benchmarks/serial_ntstore.cpp
@@ -3,7 +3,7 @@
 #include <benchmark/benchmark.h>
 #include <vector>
 
-static void benchSerialSimple(benchmark::State &state)
+static void benchSerialNTStore(benchmark::State &state)
 {
   auto [vec, index] = pad::benchmarks::makeData<long long int>(state.range(0));
   pad::benchmarks::Vec<long long int> out(state.range(0));
@@ -13,8 +13,8 @@ static void benchSerialSimple(benchmark::State &state)
     benchmark::DoNotOptimize(index.data());
     benchmark::ClobberMemory();
   }
-  // verifyScatter(vec, index, out);
+  verifyScatter(vec, index, out);
   pad::benchmarks::benchCounters(state);
 }
-BENCHMARK(benchSerialSimple)->Apply(pad::benchmarks::benchArgs)->UseRealTime();
+BENCHMARK(benchSerialNTStore)->Apply(pad::benchmarks::benchArgs)->UseRealTime();
 BENCHMARK_MAIN();
