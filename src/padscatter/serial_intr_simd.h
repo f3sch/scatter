5b4819eef584b0fc4c07ee15b517d4f8de524b78
f3sch <f3sch.git@outlook.com>
2022-02-09 14:16:43 +0100

    small fix with intrinsics
    Signed-off-by: f3sch <f3sch.git@outlook.com>

--- src/padscatter/serial_intr_simd.h
+++ src/padscatter/serial_intr_simd.h
@@ -15,15 +15,14 @@ namespace pad::serial_intr_simd
 template <typename Vec_t, typename Idx_t>
 void scatter(Vec_t &out, const Vec_t &in, const Idx_t &idx)
 {
-  assert(in.size() % 16 == 0);
-
-  for (long unsigned int out_off = 0; out_off < out.size(); out_off += 16) {
-    //load index array
-    __m512i vidx = _mm512_loadu_epi32(idx.data() + out_off);
-    //load input array
-    __m512 vin = _mm512_loadu_ps(in.data() + out_off);
-    //perform scatter
-    _mm512_i32scatter_ps(out.data() + out_off, vidx, vin, sizeof(float));
+  auto a = out.data(); // out ptr
+  auto b = idx.data(); // idx ptr
+  auto c = in.data(); // in ptr
+  auto N = idx.size(); // idx size
+  assert(in.size() % 16 == 0); // always fulfilled in our benchmarks
+  for (auto i = 0; i + 15 < N; i += 16) {
+    _mm512_i32scatter_ps(a, _mm512_loadu_epi32(b + i), _mm512_loadu_ps(c + i),
+                         4);
   }
 }
 } // namespace pad::serial_intr_simd
